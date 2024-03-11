395e145910a0e09b8abc2e420842f05fd3c58c8f
salman0909 <140049546+salman0909@users.noreply.github.com>
2024-03-11 11:18:12 +0530

    Delete springboot-crud/springboot-crud-k8s-main directory

--- springboot-crud/springboot-crud-k8s-main/src/main/java/com/javatechie/crud/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javatechie.crud.repository;
-
-import com.javatechie.crud.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
